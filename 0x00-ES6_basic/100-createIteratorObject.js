5a30196762961402524fcd9b52b02219b8ebf258
Talabi Rebecca <122875790+TemiladeRebecca@users.noreply.github.com>
2024-01-03 18:16:28 +0100

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,9 @@
 export default function createIteratorObject(report) {
-  function* iterateEmployees() {
+  return (function* _() {
     for (const department of Object.values(report.allEmployees)) {
       for (const employee of department) {
         yield employee;
       }
     }
-  }
-
-  return iterateEmployees();
+  }());
 }
