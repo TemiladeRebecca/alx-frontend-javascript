4bbabffa459992ec372bc70333748bb054aeb51f
Talabi Rebecca <122875790+TemiladeRebecca@users.noreply.github.com>
2024-01-03 19:54:56 +0100

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,9 @@
 export default function createIteratorObject(report) {
-  return Object.values(report.allEmployees).flat();
-}
-// flat() is a method that collapses multiple level
-// of nesting into one flat array which can be
-// iterated through sequentially.
+    const employees = [];
+    for (const department of Object.keys(report.allEmployees)) {
+      for (const employee of report.allEmployees[department]) {
+        employees.push(employee);
+      }
+    }
+    return employees;
+  }
