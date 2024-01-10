9dd90643594f69cd6738e559a69862c89ce52704
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-01-10 11:05:42 +0000

    Creating a student interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,32 +1,48 @@
-var student1 = {
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
     firstName: 'Reginah',
     lastName: 'Shikanda',
     age: 98,
     location: 'Ruiru',
 };
-var student2 = {
+const student2: Student = {
     firstName: 'Nancy',
     lastName: 'Chesang',
     age: 102,
     location: 'Kapsabet',
 };
-var studentsList = [student1, student2];
-var table = document.createElement('table');
-var tbody = document.createElement('tbody');
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
 table.style.background = "#CCCCFF";
 table.appendChild(tbody);
-studentsList.forEach(function (student) {
-    var row = document.createElement('tr');
-    var nameCell = document.createElement('td');
-    var locationCell = document.createElement('td');
+
+studentsList.forEach((student: Student): void => {
+    const row = document.createElement('tr');
+
+    const nameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+
     nameCell.textContent = student.firstName;
     locationCell.textContent = student.location;
+
     nameCell.style.border = "2px solid white";
     locationCell.style.border = "2px solid white";
     nameCell.style.padding = "4px";
     locationCell.style.padding = "4px";
+
     row.appendChild(nameCell);
     row.appendChild(locationCell);
     tbody.appendChild(row);
 });
+
 document.body.appendChild(table);
